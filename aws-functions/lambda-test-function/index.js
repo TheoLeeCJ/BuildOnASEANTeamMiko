23329938eebdf0e45475d3eea92477befbb096ed
TheoLeeCJ <theodorelcj@gmail.com>
2021-07-10 07:05:44 +0800

    foundation 1

--- aws-functions/lambda-test-function/index.js
+++ aws-functions/lambda-test-function/index.js
@@ -1,9 +1,13 @@
+// const 
+
 exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify({
             message: "Testing 123",
+            thingy: event,
+            body: "a"
         }),
     };
     return response;
