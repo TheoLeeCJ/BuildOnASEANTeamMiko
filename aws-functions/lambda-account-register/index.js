b9d102d9f9b5433d621470e61d4f1fbc13963c80
TheoLeeCJ <theodorelcj@gmail.com>
2021-07-11 13:30:16 +0800

    added empty field checking server-side

--- aws-functions/lambda-account-register/index.js
+++ aws-functions/lambda-account-register/index.js
@@ -3,15 +3,29 @@ const multipartParser = require("lambda-multipart-parser");
 const bcrypt = require("bcryptjs");
 const AWS = require("aws-sdk");
 
-const fields = [""];
+const fields = ["username", "password", "email"];
 
 const SECRET = "dc7bd8c0-06d6-40b0-8bcf-e09d1b4c9f76";
 
 let db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10", region: process.env.AWS_REGION });
 
 exports.handler = async (event, context, lambdaCallback) => {
+
   try {
     const formFields = await multipartParser.parse(event);
+
+    for (let i = 0; i < fields.length; i++) {
+      if (formFields[fields[i]] == "" || formFields[fields[i]] === undefined || formFields[fields[i]] == null) {
+        lambdaCallback(null, JSON.stringify({
+          statusCode: 200,
+          msg: {
+            success: false,
+            reason: "missing",
+          },
+        }));
+        return;
+      }
+    }
 
     let probe = await db.query({
       TableName: "users",
@@ -57,7 +71,7 @@ exports.handler = async (event, context, lambdaCallback) => {
   }
   catch (e) {
     lambdaCallback(null, JSON.stringify({
-      statusCode: 400,
+      statusCode: 500,
     }));
   }
 };
