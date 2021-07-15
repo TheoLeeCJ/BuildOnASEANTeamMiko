e6707f32d18091263ca20315af343ae75763155e
TheoLeeCJ <theodorelcj@gmail.com>
2021-07-16 04:17:59 +0800

    added on-demand search indexing

--- aws-functions/lambda-debug-index-refresh/index.js
+++ aws-functions/lambda-debug-index-refresh/index.js
@@ -1,25 +1,60 @@
 const AWS = require("aws-sdk"); 
 
 let s3 = new AWS.S3({ apiVersion: "2006-03-01", region: process.env.AWS_REGION });
+let db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10", region: process.env.AWS_REGION });
 
 exports.handler = async (event, context, lambdaCallback) => {
-  let index = [
-    {
-      "id": 0,
-      "cat": 0,
-      "name": "Test Item",
-    },
-  ]; 
+  let index = [];
+  let indexData, dataset;
+
+  // try {
+  //   indexData = await s3.getObject({
+  //     "Bucket": "miko-internal",
+  //     "Key": "search-index.json",
+  //   }).promise();
+  // }
+  // catch (e) {
+    
+  // }
+
+  // if (indexData) {
+  //   index = JSON.parse(indexData.toString("utf-8"));
+  // }
+
+  let scanParams = {
+    TableName: "items",
+    AttributesToGet: ["itemId", "name", "price", "category"],
+  };
 
   try {
-    let a = await s3.getObject({
-    "Bucket": "miko-internal",
-    "Key": "search-index.json",
-  }).promise();
-  
-  lambdaCallback(null, JSON.stringify(a));
+    // TODO: USE LastEvaluatedKey TO SCAN FURTHER IF 1 MB LIMIT EXCEEDED!!!
+    let response = await db.scan(scanParams).promise();
+    dataset = response.Items;
   }
   catch (e) {
-    lambdaCallback(null, e.toString())
+    lambdaCallback(null, e.toString());
+    return;
+  }
+
+  for (let i = 0; i < dataset.length; i++) {
+    index.push(
+      {
+        "id": dataset[i]["itemId"],
+        "cat": dataset[i]["category"],
+        "name": dataset[i]["name"],
+      },
+    );
+  }
+
+  try {
+    let response = await s3.putObject({
+      "Bucket": "miko-internal",
+      "Key": "search-index.json",
+      "Body": JSON.stringify(index),
+    }).promise();
+  }
+  catch (e) {
+    lambdaCallback(null, e.toString());
+    return;
   }
 };
