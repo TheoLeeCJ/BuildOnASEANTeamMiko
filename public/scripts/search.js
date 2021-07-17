9c7ea5ee689d9dcf2323ecc720ad4d3a54eb8059
Ho Wing Yip <sploooosh884@gmail.com>
2021-07-17 18:14:57 +0800

    Search for categories in category filter on search page

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -76,3 +76,163 @@ categoryFilterButton.addEventListener("click", (event) => {
     document.getElementById("category-search").focus();
   }
 });
+
+// const categoryData = await fetch("https://miko-user-img.s3.amazonaws.com/categories.json")
+//     .then((res) => res.json());
+const categoryData = {
+  "largest": 6,
+  "categories": [
+    {
+      "id": 0,
+      "name": "Cat 1",
+      "sub": [
+        { "id": 1, "name": "Cat 1 Sub 1" },
+        { "id": 4, "name": "Cat 1 Sub 2" },
+        { "id": 5, "name": "Cat 1 Sub 3" },
+      ],
+    },
+    {
+      "id": 2,
+      "name": "Cat 2",
+      "sub": [
+        { "id": 3, "name": "Cat 2 Sub 1" },
+        { "id": 6, "name": "Cat 2 Sub 2" },
+      ],
+    },
+  ],
+};
+
+const categoryOptionsDiv = document.querySelector("#category-filter .filter-options");
+for (const category of categoryData.categories) {
+  const categoryOptionDiv = document.createElement("div");
+  categoryOptionDiv.className = "filter-option";
+
+  const categoryNameSpan = document.createElement("span");
+  categoryNameSpan.className = "category-name";
+  categoryNameSpan.textContent = category.name;
+
+  const rightArrowIconSpan = document.createElement("span");
+  rightArrowIconSpan.className = "material-icons";
+  rightArrowIconSpan.textContent = "chevron_right";
+
+  const subcategoriesDiv = document.createElement("div");
+  subcategoriesDiv.className = "subcategories";
+  for (const subcategory of category.sub) {
+    const subcategoryDiv = document.createElement("div");
+    subcategoryDiv.textContent = subcategory.name;
+
+    subcategoriesDiv.append(subcategoryDiv);
+  }
+
+  categoryOptionDiv.append(categoryNameSpan, rightArrowIconSpan, subcategoriesDiv);
+  categoryOptionsDiv.append(categoryOptionDiv);
+}
+
+const categorySearchResultsDiv = document.getElementById("category-search-results");
+document.getElementById("category-search").addEventListener("input", (event) => {
+  // use querySelectorAll instead of getElementsByClassName
+  // because getElementsByClassName returns a **live** list of elements which shortens when an element is removed
+  // decreasing the length of the element list during iteration will result in some elements not being removed
+  for (const categorySearchResultDiv of document.querySelectorAll(".category-search-result")) {
+    categorySearchResultDiv.remove();
+  }
+
+  const searchQuery = event.target.value.toLowerCase();
+  if (searchQuery === "") {
+    categorySearchResultsDiv.style.display = "none";
+    for (const filterOption of document.querySelectorAll("#category-search-results ~ .filter-option")) {
+      filterOption.style.display = "flex";
+    }
+  } else {
+    categorySearchResultsDiv.style.display = "block";
+    for (const filterOption of document.querySelectorAll("#category-search-results ~ .filter-option")) {
+      filterOption.style.display = "none";
+    }
+
+    const categoryMatches = [];
+    const subcategoryMatches = [];
+    for (const category of categoryData.categories) {
+      if (category.name.toLowerCase().includes(searchQuery)) {
+        categoryMatches.push(category);
+      }
+
+      for (const subcategory of category.sub) {
+        if (subcategory.name.toLowerCase().includes(searchQuery)) {
+          subcategoryMatches.push({
+            subcategoryMatch: subcategory,
+            parentCategoryName: category.name,
+          });
+        }
+      }
+    }
+
+    const noCategorySearchResultsDiv = document
+        .getElementById("no-category-search-results");
+    if (categoryMatches.length === 0 && subcategoryMatches.length === 0) {
+      noCategorySearchResultsDiv.style.display = "block";
+    } else {
+      noCategorySearchResultsDiv.style.display = "none";
+    }
+
+    for (const { subcategoryMatch, parentCategoryName } of subcategoryMatches) {
+      // const subcategoryUrl =
+      //     `/categories/${parentCategoryName}/${subcategoryMatch.name}`;
+      // const parentCategoryUrl =
+      //     `/categories/${parentCategoryName}`;
+
+      const subcategorySearchResultDiv = document.createElement("div");
+      subcategorySearchResultDiv.className = "category-search-result";
+      // subcategorySearchResultDiv.addEventListener("click", () => {
+      //   window.location.href = subcategoryUrl;
+      // });
+      
+      const subcategoryLink = document.createElement("a");
+      subcategoryLink.className = "result-name";
+      // subcategoryLink.href = subcategoryUrl;
+      subcategoryLink.textContent = subcategoryMatch.name;
+
+      const parentCategorySpan = document.createElement("span");
+      parentCategorySpan.className = "result-parent-category";
+
+      const parentCategoryLink = document.createElement("a");
+      // parentCategoryLink.href = parentCategoryUrl;
+      parentCategoryLink.textContent = parentCategoryName;
+      
+      parentCategorySpan.append("in ", parentCategoryLink);
+
+      // const rightArrowIconDiv = document.createElement("div");
+      // rightArrowIconDiv.className = "material-icons";
+      // const rightArrowIconSpan = document.createElement("span");
+      // rightArrowIconSpan.textContent = "chevron_right";
+      // rightArrowIconDiv.append(rightArrowIconSpan);
+
+      subcategorySearchResultDiv.append(subcategoryLink, parentCategorySpan);
+      
+      categorySearchResultsDiv.append(subcategorySearchResultDiv);
+    }
+    for (const categoryMatch of categoryMatches) {
+      // const categoryUrl = `/categories/${categoryMatch.name}`;
+
+      const categorySearchResultDiv = document.createElement("div");
+      categorySearchResultDiv.className = "category-search-result";
+      // categorySearchResultDiv.addEventListener("click", () => {
+      //   window.location.href = categoryUrl;
+      // });
+
+      const categoryLink = document.createElement("a");
+      categoryLink.className = "result-name";
+      // categoryLink.href = categoryUrl;
+      categoryLink.textContent = categoryMatch.name;
+
+      // const rightArrowIconDiv = document.createElement("div");
+      // rightArrowIconDiv.className = "material-icons";
+      // const rightArrowIconSpan = document.createElement("span");
+      // rightArrowIconSpan.textContent = "chevron_right";
+      // rightArrowIconDiv.append(rightArrowIconSpan);
+
+      categorySearchResultDiv.append(categoryLink);
+
+      categorySearchResultsDiv.append(categorySearchResultDiv);
+    }
+  }
+});
