c5b0a3a021ec6a97fe815ca104792f6afed73090
Ho Wing Yip <sploooosh884@gmail.com>
2021-07-22 16:29:00 +0800

    Profile page user listings UI

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,11 +1,22 @@
 const profileTabSwitchers = document.getElementsByClassName("profile-tab-switcher");
-for (const profileTabSwitcher of profileTabSwitchers) {
+const profileTabContentDivs = document.getElementsByClassName("profile-tab-content");
+for (let i = 0; i < profileTabSwitchers.length; i++) {
+  const profileTabSwitcher = profileTabSwitchers[i];
   profileTabSwitcher.addEventListener("click", () => {
     for (const otherProfileTabSwitcher of profileTabSwitchers) {
       if (otherProfileTabSwitcher === profileTabSwitcher) {
         otherProfileTabSwitcher.classList.add("profile-tab-switcher-selected");
       } else {
         otherProfileTabSwitcher.classList.remove("profile-tab-switcher-selected");
+      }
+    }
+
+    for (let j = 0; j < profileTabContentDivs.length; j++) {
+      if (j == i) {
+        profileTabContentDivs[j].classList.add("profile-tab-content-selected");
+      } else {
+        profileTabContentDivs[j].classList
+            .remove("profile-tab-content-selected");
       }
     }
   });
